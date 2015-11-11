12cc58cb2c58e0ec82dd5c83892e83f338e343f2
Yi-wen LIN <bongiovi015@gmail.com>
2015-11-11 17:30:12 +0000

    emg 02 wip
    Signed-off-by: Yi-wen LIN <bongiovi015@gmail.com>

---
+++ labs/wen/emergency/02/src/js/SceneApp.js
@@ -0,0 +1,124 @@
+// SceneApp.js
+
+var GL        = bongiovi.GL, gl;
+var ViewBall  = require("./ViewBall");
+var ViewTrace = require("./ViewTrace");
+var Ball      = require("./Ball");
+var vec3      = bongiovi.glm.vec3;
+var random    = function(min, max) { return min + Math.random() * (max - min);	}
+
+function SceneApp() {
+	gl = GL.gl;
+	bongiovi.Scene.call(this);
+	this.resize();
+	this._initBalls();
+
+	window.addEventListener("resize", this.resize.bind(this));
+}
+
+
+var p = SceneApp.prototype = new bongiovi.Scene();
+
+p._initTextures = function() {
+	console.log('Init Textures');
+	this._texture = new bongiovi.GLTexture(images.light);
+};
+
+p._initViews = function() {
+	console.log('Init Views');
+	this._vAxis     = new bongiovi.ViewAxis();
+	this._vDotPlane = new bongiovi.ViewDotPlane();
+	this._vBall     = new ViewBall();
+	this._vTrace    = new ViewTrace();
+};
+
+p._initBalls = function() {
+	var numBalls = params.numBubble;
+	var range = 100;
+	this._balls = [];
+	for(var i=0; i<numBalls; i++) {
+		var b = new Ball();
+		b.size = random(40, 80);
+		b.position = vec3.fromValues(random(-range, range), random(-range, range), random(-range, range));
+
+		this._balls.push(b);
+	}
+
+	// this._checkPosition();
+};
+
+
+p._checkPosition = function() {
+	function dir(a, b) {
+		var d = vec3.create();
+		vec3.sub(d, a.position, b.position);
+
+		return d;
+	}
+
+	function distance(a, b) {
+		var d = dir(a, b);
+		return vec3.length(d);
+	}
+
+	function touched(a, b) {
+		var dist = distance(a, b);
+		if(dist < a.size + b.size) {
+			return true;
+		}
+
+		return false;
+	}
+
+	function push(a, b) {
+		console.log('push');
+		var d = dir(a, b);
+		vec3.normalize(d, d);
+		var dist = distance(a, b);
+		vec3.scale(d, d, dist);
+
+		vec3.add(b.position, a.position, d);
+	}
+
+
+
+	var ballCurr, ballTarget;
+	for(var i=0; i<this._balls.length-1; i++) {
+		ballCurr = this._balls[i];
+		for(var j=i+1; j<this._balls.length; j++) {
+			ballTarget = this._balls[j];
+			if(ballTarget) {
+				if(touched(ballCurr, ballTarget)) {
+					push(ballCurr, ballTarget);
+				}	
+			}
+			
+		}
+	}
+};
+
+p.render = function() {
+	if(!this._balls) return;
+	this._vAxis.render();
+	this._vDotPlane.render();
+
+	for(var i=0; i<this._balls.length; i++) {
+		var b = this._balls[i];
+		this._vBall.render(b.position, b.size);	
+	}
+
+	GL.clear(0, 0, 0, 0);
+
+	GL.setMatrices(this.cameraOrtho);
+	GL.rotate(this.rotationFront);
+
+	this._vTrace.render(this._balls, this._texture);
+	
+};
+
+p.resize = function() {
+	GL.setSize(window.innerWidth, window.innerHeight);
+	this.camera.resize(GL.aspectRatio);
+};
+
+module.exports = SceneApp;
---
+++ labs/wen/emergency/02/src/js/app.js
@@ -0,0 +1,55 @@
+// app.js
+window.bongiovi = require("./libs/bongiovi.js");
+var dat = require("dat-gui");
+window.params = {
+	focus:1.5,
+	numIter:100,
+	numBubble:10.0,
+	metaK:7.0,
+	zGap:2.0,
+	maxDist:5.0
+};
+
+(function() {
+	var SceneApp = require("./SceneApp");
+
+	App = function() {
+		var l = new bongiovi.SimpleImageLoader();
+		var a = [ "assets/light.jpg" ];
+
+		l.load(a, this, this._onLoaded);
+	}
+
+	var p = App.prototype;
+
+	p._onLoaded = function(img) {
+		window.images = img;
+
+		if(document.body) this._init();
+		else {
+			window.addEventListener("load", this._init.bind(this));
+		}
+	};
+
+	p._init = function() {
+		this.canvas = document.createElement("canvas");
+		this.canvas.width = window.innerWidth;
+		this.canvas.height = window.innerHeight;
+		this.canvas.className = "Main-Canvas";
+		document.body.appendChild(this.canvas);
+		bongiovi.GL.init(this.canvas);
+
+		this._scene = new SceneApp();
+		bongiovi.Scheduler.addEF(this, this._loop);
+
+		// this.gui = new dat.GUI({width:300});
+	};
+
+	p._loop = function() {
+		this._scene.loop();
+	};
+
+})();
+
+
+new App();
