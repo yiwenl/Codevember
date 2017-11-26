fff347b5c6a24ad3e4bb20f7fed3dd3ce61c6723
wen <bongiovi015@gmail.com>
2017-11-26 18:09:39 +0000

    27

--- 2017/experiments/27/src/js/asset-list.js
+++ 2017/experiments/27/src/js/asset-list.js
@@ -15,16 +15,7 @@ const assetsToLoad = [
 	{"id":"wolf13","url":"assets/obj/wolf13.obj","type":"text"},
 	{"id":"wolf14","url":"assets/obj/wolf14.obj","type":"text"},
 	{"id":"wolf15","url":"assets/obj/wolf15.obj","type":"text"},
-	{"id":"wolf16","url":"assets/obj/wolf16.obj","type":"text"},
-	{"id":"aomap","url":"assets/img/aomap.jpg","type":"jpg"},
-	{"id":"irr_negx","url":"assets/img/irr_negx.hdr","type":"binary"},
-	{"id":"irr_negy","url":"assets/img/irr_negy.hdr","type":"binary"},
-	{"id":"irr_negz","url":"assets/img/irr_negz.hdr","type":"binary"},
-	{"id":"irr_posx","url":"assets/img/irr_posx.hdr","type":"binary"},
-	{"id":"irr_posy","url":"assets/img/irr_posy.hdr","type":"binary"},
-	{"id":"irr_posz","url":"assets/img/irr_posz.hdr","type":"binary"},
-	{"id":"noise","url":"assets/img/noise.png","type":"png"},
-	{"id":"studio_radiance","url":"assets/img/studio_radiance.dds","type":"binary"}
+	{"id":"wolf16","url":"assets/obj/wolf16.obj","type":"text"}
 ];
 
 
