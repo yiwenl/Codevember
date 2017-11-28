3ef1b4fe69538c72241ff40039e9f1ff6a3e8f9b
wen <bongiovi015@gmail.com>
2017-11-28 16:22:57 +0000

    add lamp

---
+++ 2017/experiments/_backups/lamps/src/js/ViewTest.js
@@ -0,0 +1,56 @@
+// ViewTest.js
+
+import alfrid, { GL } from 'alfrid';
+import vs from 'shaders/test.vert';
+import fs from 'shaders/test.frag';
+
+var random = function(min, max) { return min + Math.random() * (max - min);	}
+
+class ViewTest extends alfrid.View {
+	
+	constructor() {
+		super(vs, fs);
+	}
+
+
+	_init() {
+		const s = 2;
+		this.diskSize = s;
+		this.mesh = alfrid.Geom.plane(s, s, 100, 'xz');
+
+		let r = 10;
+
+		const getPos = () => {
+			return [random(-r/2, r/2), random(2, 5), random(-r, r)];
+		}
+
+
+		const positions = [];
+		const extras = [];
+		const numLamps = 30;
+
+		for(let i=0; i<numLamps; i++) {
+			positions.push(getPos());
+			extras.push([random(1, 2), Math.random(), random(.5, 2)])
+		}
+
+		this.mesh.bufferInstance(positions, 'aPosOffset');
+		this.mesh.bufferInstance(extras, 'aExtra');
+
+
+		this.positions = positions;
+
+	}
+
+
+	render() {
+		this.shader.bind();
+		this.shader.uniform("uTime", "float", alfrid.Scheduler.deltaTime);
+		this.shader.uniform("uSize", "float", this.diskSize);
+		GL.draw(this.mesh);
+	}
+
+
+}
+
+export default ViewTest;
---
+++ 2017/experiments/_backups/lamps/src/shaders/test.frag
@@ -0,0 +1,20 @@
+// copy.frag
+
+#extension GL_OES_standard_derivatives : enable
+
+precision highp float;
+varying vec2 vTextureCoord;
+varying vec3 vOldPos;
+varying vec3 vNewPos;
+
+void main(void) {
+
+	float d = distance(vTextureCoord, vec2(.5));
+	d = smoothstep(.5, .3, d);
+
+	float oldArea = length(dFdx(vOldPos)) * length(dFdy(vOldPos));
+	float newArea = length(dFdx(vNewPos)) * length(dFdy(vNewPos));
+
+	float g = oldArea / newArea * 0.2;
+	gl_FragColor = vec4(vec3(g), 1.0) * d;
+}
