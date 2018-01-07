df368abd158dfe3ddf9427bf0126be0d390aefba
wen <bongiovi015@gmail.com>
2018-01-07 23:53:59 +0000

    Add comments

--- 2017/breakdowns/01/src/js/SceneApp.js
+++ 2017/breakdowns/01/src/js/SceneApp.js
@@ -15,12 +15,13 @@ class SceneApp extends Scene {
 		super();
 		this.resize();
 		GL.enableAlphaBlending();
-		this.orbitalControl.rx.value = this.orbitalControl.ry.value = 0.3;
+
+		//	Adjust the camera control, disable the zooming
 		this.orbitalControl.radius.value = radius;
 		this.orbitalControl.radius.limit(radius, radius);
 		this.camera.setPerspective(FOV * RAD, GL.aspectRatio, .1, 50);
 
-
+		//	Setup a camera from the imagine light source
 		this.cameraPortrait = new CameraPerspective();
 		this.cameraPortrait.setPerspective(FOV * RAD, ratio, .1, 50);
 
@@ -34,7 +35,7 @@ class SceneApp extends Scene {
 			0.5, 0.5, 0.5, 1.0
 		);
 
-
+		//	Getting the shadow matrix from this camera
 		this.shadowMatrix = mat4.create();
 		mat4.multiply(this.shadowMatrix, this.cameraPortrait.projection, this.cameraPortrait.viewMatrix);
 		mat4.multiply(this.shadowMatrix, this.biasMatrix, this.shadowMatrix);
@@ -43,6 +44,7 @@ class SceneApp extends Scene {
 
 	_initTextures() {
 		console.log('init textures');
+		//	Getting the portrait texture
 		this.texturePortrait = Assets.get('p01');
 	}
 
@@ -50,13 +52,18 @@ class SceneApp extends Scene {
 	_initViews() {
 		console.log('init views');
 
+		//	Initialise the shader
 		this.shader = new alfrid.GLShader(vs, fs);
+
+		//	Load in the cyliner obj file
 		this.mesh = Assets.get('cylinder');
 		const numPillars = 80;
 		const positions = [];
 		const extras = [];
 		const {sin, cos, random, PI} = Math;
 
+
+		//	get random position within a circle
 		function getPos() {
 			let t = 2 * PI * random();
 			let u = random() + random();
@@ -65,6 +72,8 @@ class SceneApp extends Scene {
 			return [r*cos(t), 0, r*sin(t)]
 		}
 
+
+		//	setup the instancing
 		for(let i=0; i<numPillars; i++) {
 			positions.push(getPos());
 		}
@@ -76,11 +85,14 @@ class SceneApp extends Scene {
 	render() {
 		GL.clear(0, 0, 0, 0);
 
+		//	binding shader then setup the uniforms
 		this.shader.bind();
 		this.shader.uniform("uShadowMatrix", "mat4", this.shadowMatrix);
 		this.shader.uniform("texturePortrait", "uniform1i", 1);
 		this.texturePortrait.bind(1);
 		this.shader.uniform("uRatio", "float", ratio);
+
+		//	draw the mesh
 		GL.draw(this.mesh);
 	}
 
